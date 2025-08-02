433577edd01e665c5165af9bd1c69498a2e010d9
borvlados <124503283+borvlados@users.noreply.github.com>
2025-08-02 11:27:03 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,56 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    // додаємо в список живих тварин
+    Animal.alive.push(this);
+  }
+
+  // допоміжний метод, який видаляє тварину зі списку живих
+  die() {
+    const index = Animal.alive.indexOf(this);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // Не працює, якщо ціль - інший хижак
+    if (target instanceof Carnivore) {
+      return;
+    }
+
+    // Не працює, якщо травоїдна тварина сховалась
+    if (target.hidden) {
+      return;
+    }
+
+    // Зменшуємо здоров'я на 50
+    target.health -= 50;
+
+    // Якщо здоров'я <= 0, видаляємо із alive
+    if (target.health <= 0) {
+      target.die();
+    }
+  }
 }
 
 module.exports = {
